6692c574837db295dd63a0891c7559e87d7c7915
Vik_666@mail.ru <ViktorVLapin@gmail.com>
2009-09-30 19:39:00 +0000

    Optional connection.

--- client-java/src/Connection/Connector.java
+++ client-java/src/Connection/Connector.java
@@ -3,10 +3,7 @@ package Connection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.*;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.OutputStreamWriter;
-import java.io.FileOutputStream;
+import java.io.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,28 +15,54 @@ import java.io.FileOutputStream;
 public class Connector{
 
     URL Url;
+    URLConnection connection;
 
     public Connector(String adress) throws IOException {
         Url = new URL(adress);
     }
 
-    public void GetFileFromURL() throws IOException {
+    public void GetFileFromURL() {
+        
+        try {
+            connection = Url.openConnection();
+            PrintWriter pw = new PrintWriter   //
+                (new OutputStreamWriter          // -
+                (new FileOutputStream         //
+                ("answer_file.xml"), "utf-8"));
 
-        URLConnection connection;
-        connection = Url.openConnection();
+            int i = 0;
+            while (i != connection.getContentLength()){
+                pw.print((char)connection.getInputStream().read());
+                ++i;
+            }
+            pw.close();
+        } catch (IOException e) {
+            try {
+                BufferedReader bReader = new BufferedReader (new InputStreamReader(System.in)); 
+                System.out.println("input IP (192.168.0.2 for APTU)");
+                String IP = bReader.readLine();
+                System.out.println("input port (3128 for APTU)");
+                int port = Integer.parseInt(bReader.readLine());
+                connection = Url.openConnection(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(IP, port)));
+                
+                PrintWriter pw = new PrintWriter   //
+                    (new OutputStreamWriter          // -
+                    (new FileOutputStream         //
+                    ("answer_file.xml"), "utf-8"));
+
+                int i = 0;
+                while (i != connection.getContentLength()){
+                    pw.print((char)connection.getInputStream().read());
+                    ++i;
+                }
+                pw.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
         //connection = Url.openConnection(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.0.2", 3128)));
 
-        PrintWriter pw = new PrintWriter   //     
-            (new OutputStreamWriter          // -
-            (new FileOutputStream         //     
-            ("answer_file.xml"), "utf-8"));
 
-        int i = 0;
-        while (i != connection.getContentLength()){
-            pw.print((char)connection.getInputStream().read());
-            ++i;
-        }
-        pw.close();
 
     }
 
