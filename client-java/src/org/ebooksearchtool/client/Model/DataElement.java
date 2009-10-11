ce1392ab107a2b75cf9c9114db3f7f5631bd8645
Vik_666@mail.ru <ViktorVLapin@gmail.com>
2009-10-11 08:13:48 +0000

    java-client: model

---
+++ client-java/src/org/ebooksearchtool/client/Model/DataElement.java
@@ -0,0 +1,21 @@
+package Model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Администратор
+ * Date: 01.10.2009
+ * Time: 22:59:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataElement {
+
+    private String[] fields;
+
+    public DataElement(){
+        fields = new String[5];
+    }
+
+    public String[] getFields() {
+        return fields;
+    }
+}
