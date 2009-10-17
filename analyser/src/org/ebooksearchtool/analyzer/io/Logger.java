8a1015be89f6a4f6a4333a9281c2f996355df9cb
xboctus <xboctus@gmail.com>
2009-10-17 11:32:36 +0000

    analyzer: Added normal logger, TODO changed.

--- analyser/src/org/ebooksearchtool/analyzer/io/Logger.java
+++ analyser/src/org/ebooksearchtool/analyzer/io/Logger.java
@@ -1,6 +1,12 @@
 package org.ebooksearchtool.analyzer.io;
 
 import java.io.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 
 /**
  * @author Алексей
@@ -8,19 +14,40 @@ import java.io.*;
 
 public class Logger {
 
-    //TODO: Изменить логгер так, что бы он не пререписывал лог каждый раз заново
     public static void setToLog(String message){
-        BufferedWriter out = null;
+        String s = "\\log\\log" + getCurrentDate() + ".txt";
+        RandomAccessFile log = null;
         try{
             try{
-                out = new BufferedWriter(new FileWriter("log.txt"));
-                out.write(message);
-                out.newLine();
-            }catch (IOException ex){
-
-            }finally{
-                out.close();
-            }
+                log = new RandomAccessFile(s, "rws");
+                long length = log.length();
+                log.readFully(new byte[(int)length]);
+                log.writeBytes(getCurrentTime());
+                log.writeBytes(System.getProperty("line.separator"));
+                log.writeBytes(message);
+                log.writeBytes(System.getProperty("line.separator"));
+                log.writeBytes(System.getProperty("line.separator"));
+                } catch (IOException ex) {
+                    System.out.print(ex.toString());
+                }finally{
+                    log.close();
+                }
         }catch(IOException ex){}
+    }
+
+    private static String getCurrentDate(){
+        TimeZone zone = new SimpleTimeZone(3, "Moscow");
+        GregorianCalendar calendar = new GregorianCalendar(zone);
+        return calendar.get(Calendar.YEAR)
+               + "_" + calendar.get(Calendar.MONTH)
+               + "_" + calendar.get(Calendar.DATE);
+    }
+
+    private static String getCurrentTime(){
+        TimeZone zone = new SimpleTimeZone(3, "Moscow");
+        GregorianCalendar calendar = new GregorianCalendar(zone);
+        return calendar.get(Calendar.HOUR_OF_DAY)
+               + ":" + calendar.get(Calendar.MINUTE)
+               + ":" + calendar.get(Calendar.SECOND);
     }
 }
