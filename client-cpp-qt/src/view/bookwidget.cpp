ae5108b113fd3b118064aba76587394f140f5d27
cvetlana.mail <cvetlana.mail@gmail.com>
2009-11-02 14:35:46 +0000

    client-cpp: images moved, book representation changed.

--- client-cpp-qt/src/view/bookwidget.cpp
+++ client-cpp-qt/src/view/bookwidget.cpp
@@ -1,6 +1,5 @@
 #include <QLabel>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
+#include <QGridLayout>
 #include <QUrl>
 #include <QFile>
 #include <QFileInfo>
@@ -23,32 +22,28 @@ BookWidget::BookWidget(QWidget* parent, const Book* book) : QWidget(parent) ,myB
     myCheckBox = new QCheckBox();
     myTitleLabel = new QLabel(myBook->getTitle().c_str());
     myAuthorLabel = new QLabel(myBook->getAuthor()->getName().c_str());
-	QVBoxLayout* layout = new QVBoxLayout();
-	layout->addWidget(myTitleLabel);
-	layout->addWidget(myAuthorLabel);
-
-//    myButtonGroup = new QButtonGroup();    
-    QIcon* deleteIcon = new QIcon("view/images/delete.jpeg");
-    QPushButton* deleteButton = new QPushButton(*deleteIcon, "", this);    
-    QIcon* toLibraryIcon = new QIcon("view/images/tolibrary.jpeg");
-    QPushButton* toLibraryButton = new QPushButton(*toLibraryIcon, "", this);    
-    QIcon* readIcon = new QIcon("view/images/read.jpeg");
-    QPushButton* readButton = new QPushButton(*readIcon, "", this);    
-   // myButtonGroup->addButton(deleteButton, 1);
-    //myButtonGroup->addButton(toLibraryButton, 2);
-    //myButtonGroup->addButton(readButton, 3);
-   
-    QHBoxLayout* mainLayout = new QHBoxLayout();
+    QString summary(myBook->getTitle().c_str());
+    summary.prepend("Summary: ");    
     QLabel* cover = new QLabel("COVER");
-//    QLabel* buttons = new QLabel("BUTTONS");
-   
-    mainLayout->addWidget(myCheckBox);
-	mainLayout->addWidget(cover);
-    mainLayout->addLayout(layout);
-	//mainLayout->addWidget(buttons);
-    mainLayout->addWidget(deleteButton);
-    mainLayout->addWidget(toLibraryButton);
-    mainLayout->addWidget(readButton);
+
+    QIcon* deleteIcon = new QIcon("view/images/delete.jpeg");
+    QIcon* toLibraryIcon = new QIcon("view/images/tolibrary.jpeg");
+    QIcon* readIcon = new QIcon("view/images/read.jpeg");
+
+    QPushButton* deleteButton = new QPushButton(*deleteIcon, "", this);    
+    QPushButton* toLibraryButton = new QPushButton(*toLibraryIcon, "", this);    
+    QPushButton* readButton = new QPushButton(*readIcon, "", this);    
+	
+    QGridLayout* mainLayout = new QGridLayout();
+
+    mainLayout->addWidget(myCheckBox, 0, 0, 3, 1);
+    mainLayout->addWidget(cover, 0, 1, 3, 1);
+    mainLayout->addWidget(myTitleLabel, 0, 2);
+    mainLayout->addWidget(myAuthorLabel, 1, 2);
+    //mainLayout->addWidget(summaryLabel, 2, 2);
+    mainLayout->addWidget(deleteButton, 0, 3);
+    mainLayout->addWidget(toLibraryButton, 1, 3);
+    mainLayout->addWidget(readButton, 2, 3);
 
     setLayout(mainLayout);
 }
