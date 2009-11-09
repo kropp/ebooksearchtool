884ededa0f608ef25a41140677efb002ff7f854c
Tuzova.K <Tuzova.K@gmail.com>
2009-11-09 08:11:48 +0000

    server: fixed admin(test bug)

---
+++ server/book/admin.py
@@ -0,0 +1,15 @@
+from server.book.models import *
+
+admin.site.register(Annotation)
+
+admin.site.register(Book)
+
+admin.site.register(Author)
+
+admin.site.register(BookFile)
+
+admin.site.register(Series)
+
+admin.site.register(Alias)
+
+admin.site.register(Tag)
--- server/book/models.py
+++ server/book/models.py
@@ -72,16 +72,4 @@ class Author(models.Model):
     def __unicode__(self):
         return self.name
         
-#admin.site.register(Annotation)
-#
-#admin.site.register(Book)
-#
-#admin.site.register(Author)
-#
-#admin.site.register(BookFile)
-#
-#admin.site.register(Series)
-#
-#admin.site.register(Alias)
-#
-#admin.site.register(Tag)
+
