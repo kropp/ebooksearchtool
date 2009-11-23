06f38c1b5779853f6eea8b8872fec76ce01de099
Tuzova.K <Tuzova.K@gmail.com>
2009-11-23 14:19:20 +0000

    server: few changes

--- server/urls_root.py
+++ server/urls_root.py
@@ -16,28 +16,41 @@ urlpatterns = patterns('',
     (r'^data/insert/?$', data_modify, {'action': ACTION['insert'],}),
 
     # interface for search
-    (r'^search.atom/?$', 'views.search_request_to_server', {'response_type': 'atom',}),
-    (r'^search/?$', 'views.search_request_to_server', {'response_type': 'xhtml',}),
+    (r'^search.atom/?$', 'views.search_request_to_server', 
+        {'response_type': 'atom',}),
+    (r'^search/?$', 'views.search_request_to_server', 
+        {'response_type': 'xhtml',}),
     
-    (r'^book.atom/id(\d{1,})/?$', 'views.book_request_to_server', {'response_type': 'atom',}),
-    (r'^book/id(\d{1,})/?$', 'views.book_request_to_server', {'response_type': 'xhtml',}),
+    (r'^book.atom/id(\d{1,})/?$', 'views.book_request_to_server', 
+        {'response_type': 'atom',}),
+    (r'^book/id(\d{1,})/?$', 'views.book_request_to_server', 
+        {'response_type': 'xhtml',}),
     
-    (r'^author.atom/id(\d{1,})/?$', 'views.author_request_to_server', {'response_type': 'atom',}),
-    (r'^author/id(\d{1,})/?$', 'views.author_request_to_server', {'response_type': 'xhtml',}),
+    (r'^author.atom/id(\d{1,})/?$', 'views.author_request_to_server', 
+        {'response_type': 'atom',}),
+    (r'^author/id(\d{1,})/?$', 'views.author_request_to_server', 
+        {'response_type': 'xhtml',}),
     
-    (r'^authors/id(\d{1,})/books.atom?$', 'server.views.author_books_request_to_server', {'response_type': 'atom',}),
+    (r'^authors/id(\d{1,})/books.atom?$', 'views.author_books_request_to_server', 
+        {'response_type': 'atom',}),
     
     #all books
-    (r'^all.atom/?$', 'views.all_books_request_to_server', {'response_type': 'atom',}),
-    (r'^all/?$', 'views.all_books_request_to_server', {'response_type': 'xhtml',}),
+    (r'^all.atom/?$', 'views.all_books_request_to_server', 
+        {'response_type': 'atom',}),
+    (r'^all/?$', 'views.all_books_request_to_server', 
+        {'response_type': 'xhtml',}),
     
     #book catalog
-    (r'^catalog.atom/?$', 'server.views.catalog_request_to_server', {'response_type': 'atom',}),
+    (r'^catalog.atom/?$', 'server.views.catalog_request_to_server', 
+        {'response_type': 'atom',}),
     
     #books sorted by authors, languages, subjects
-    (r'^discover/authors.atom/?$', 'server.views.books_by_authors_request_to_server', {'response_type': 'atom',}),
-    (r'^discover/languages.atom/?$', 'server.views.books_by_languages_request_to_server', {'response_type': 'atom',}),
-    (r'^discover/subjects.atom/?$', 'server.views.books_by_tags_request_to_server', {'response_type': 'atom',}),
+    (r'^discover/authors.atom/?$', 'views.books_by_authors_request_to_server', 
+        {'response_type': 'atom',}),
+    (r'^discover/languages.atom/?$', 'views.books_by_languages_request_to_server', 
+        {'response_type': 'atom',}),
+    (r'^discover/subjects.atom/?$', 'views.books_by_tags_request_to_server', 
+        {'response_type': 'atom',}),
     
     #admin
     (r'^admin/(.*)', admin.site.root),
@@ -50,7 +63,8 @@ urlpatterns = patterns('',
 #    (r'^user/insert/?$', 'reader.views.insert_user_information'),
     
     #openid
-    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    (r'^$', 'django.views.generic.simple.direct_to_template', 
+        {'template': 'home.html'}),
     (r'^account/', include('django_authopenid.urls')),
 
     
