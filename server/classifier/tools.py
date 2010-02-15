d39606236cd22e85d62d95b741a3422b26c9287d
Tuzova.K <Tuzova.K@gmail.com>
2010-02-15 17:55:34 +0000

    server: added pdf reader for new classifier

--- server/classifier/tools.py
+++ server/classifier/tools.py
@@ -2,43 +2,44 @@
 
 import re
 import spec.external.feedparser as feedparser
+import spec.external.pyPdf as pyPdf
 import classifier
-import pickle
+import urllib
 
 def read(feed, classifier):
     ''' gets URL and classify items '''
     # get feed items
     f = feedparser.parse(feed)
+#    file_handle = open("statistics", "a")
     
     for entry in f['entries']:
         summary = entry['summary'].encode('utf-8')
-        if summary != "No description available.":
-            print 
-            print '-----'
+        if summary == "No description available.":
+            break
+        print 
+        print '-----'
 #            print 'Title: ' + entry['title'].encode('utf-8')
 #            print 'Author: ' + entry['author'].encode('utf-8')
 #            print
 #            print summary
-            
-            fulltext = '%s\n%s' % (entry['title'].encode('utf-8'), summary)
-
-            hyp = classifier.classify(fulltext)
-            print 'Hypothesis: ' + str(hyp)
-                        
-            for cat in entry['categories']:
-                c = cat[1].encode('utf-8')
-                print ' Tag : ' + c
-                classifier.train(fulltext, c)
-                
-                if c == hyp[0] or c == hyp[1]:
-                    write_statistics(c)
         
-def write_statistics(tag_name):
-    file_handle = open("statistics", "a")
-    file_handle.write(tag_name)
-    file_handle.write('\n')    
-    file_handle.close()
-    
+        fulltext = '%s\n%s' % (entry['title'].encode('utf-8'), summary)
+
+        hyp = classifier.classify(fulltext)
+        print 'Hypothesis: ' + str(hyp)
+                    
+        for cat in entry['categories']:
+            c = cat[1].encode('utf-8')
+            print ' Tag : ' + c
+            classifier.train(fulltext, c)
+            
+            # write statistic
+#                if c == hyp[0] or c == hyp[1]:     
+#                    file_handle.write(tag_name)
+#                    file_handle.write('\n')    
+
+#    file_handle.close()
+        
 def get_statistics():
     file_handle = open("statistics", "r")
     lines = file_handle.readlines()
@@ -59,3 +60,39 @@ def get_statistics():
         print l, count
     
     print "Total: ", total
+    
+def classify_fullbook(feed, classif):
+    f = feedparser.parse(feed)
+    
+    for entry in f['entries']:
+        url = entry['id'].encode('utf-8') + ".pdf"
+        webFile = urllib.urlopen(url)
+        localFile = open("downloads/" + url.split('/')[-1], 'w')
+        localFile.write(webFile.read())
+        fulltext = webFile.read()
+        webFile.close()
+        localFile.close()
+
+        content = ""
+        # Load PDF into pyPDF
+        pdf = pyPdf.PdfFileReader(file("downloads/" + url.split('/')[-1], "rb"))
+        # Iterate pages
+        for i in range(0, pdf.getNumPages()):
+            # Extract text from page and add to content
+            content += pdf.getPage(i).extractText() + "\n"
+        # Collapse whitespace
+        content = " ".join(content.replace(u"\xa0", " ").strip().split())
+
+        print 
+        print '-----'
+        print content
+
+
+        #hyp = classif.classify(fulltext)
+        #print 'Hypothesis: ' + str(hyp)
+                    
+        #for cat in entry['categories']:
+        #    c = cat[1].encode('utf-8')
+        #    print ' Tag : ' + c
+        #    classif.train(fulltext, c)
+           
