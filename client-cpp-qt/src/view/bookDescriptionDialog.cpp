e5a18da600b2508e3300dde750acb5d03a0b150f
cvetlana.mail <cvetlana.mail@gmail.com>
2010-02-16 09:19:06 +0000

    client-cpp: html-content is displayed correctly.

--- client-cpp-qt/src/view/bookDescriptionDialog.cpp
+++ client-cpp-qt/src/view/bookDescriptionDialog.cpp
@@ -20,23 +20,25 @@ BookDescriptionDialog(QWidget* parent, const Book& book)
 }
  
 void BookDescriptionDialog::setText(QTextEdit* text, const Book& book) {
-    QString textContent(tr("Title: "));
-    textContent.append(book.getTitle());
-    textContent.append(tr("\nAuthor: "));
+    QString textContent("<p><b>" + tr(" Title: ") + "</b>");
+    textContent.append(book.getTitle() + "</p>");
+    textContent.append("<p><b>" + tr("Author: ") + "</b>");
     QVector <const Author*> authors = book.getAuthors();
     foreach (const Author* author, authors) {
         textContent.append(author->getName() + "  ");
     }
-    
+    textContent.append("</p>");
     if (!book.getLanguage().isEmpty())
-        textContent.append(tr("\nLanguage: ") + book.getLanguage());
+        textContent.append("<p><b>" + tr("\nLanguage: ") + "</b>" + book.getLanguage() + "</p>");
     
     if (!book.getSummary().isEmpty())
-        textContent.append(tr("\nSummary: ") + book.getSummary());
+        textContent.append("<p> <b>" + tr("\nSummary: ") + "</b>" + book.getSummary() + "</p>");
     
-    if (!book.getContent().isEmpty())
-        textContent.append(tr("\nContent: ") + book.getContent());
-        
-    text->setText(textContent);
+    if (!book.getContent().isEmpty()) {
+        textContent.append("</p><b>" + tr("\nContent: ") + "</b>");
+        textContent.append(book.getContent() + "</p>");
+    }
+
+    text->insertHtml(textContent);       
     text->setReadOnly(true);
 }
