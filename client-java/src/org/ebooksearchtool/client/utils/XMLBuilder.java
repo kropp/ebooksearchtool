4713e6156e4fc29a85aa26bc539efb89f0a34efe
Vik_666@mail.ru <ViktorVLapin@gmail.com>
2010-02-23 00:05:08 +0000

    client-java: serialisation improved

--- client-java/src/org/ebooksearchtool/client/utils/XMLBuilder.java
+++ client-java/src/org/ebooksearchtool/client/utils/XMLBuilder.java
@@ -57,52 +57,106 @@ public class XMLBuilder {
         doc.appendChild(root);
 		
 		for(int i = 0; i < data.getBooks().size(); ++i){
+
 			Element entry = doc.createElement("entry");
-			Element title = doc.createElement("title");
+
+            Element title = doc.createElement("title");
 			title.setTextContent(data.getBooks().get(i).getTitle());
-			Element id = doc.createElement("id");
+            entry.appendChild(title);
+
+            if (data.getBooks().get(i).getSubtitle() != null) {
+                Element subtitle = doc.createElement("subtitle");
+                subtitle.setTextContent(data.getBooks().get(i).getSubtitle());
+                entry.appendChild(subtitle);
+            }
+
+            Element id = doc.createElement("id");
 			id.setTextContent(data.getBooks().get(i).getID());
-			Element author = doc.createElement("author");
-			Element name = doc.createElement("name");
-			name.setTextContent(data.getBooks().get(i).getAuthor().getName());
-			Element AID = doc.createElement("uri");
-			AID.setTextContent(data.getBooks().get(i).getAuthor().getID());
-			Element language = doc.createElement("dcterms:language");
-			language.setTextContent(data.getBooks().get(i).getLanguage());
+            entry.appendChild(id);
+
+            if (data.getBooks().get(i).getAuthor() != null) {
+                Element author = doc.createElement("author");
+                Element name = doc.createElement("name");
+                name.setTextContent(data.getBooks().get(i).getAuthor().getName());
+                author.appendChild(name);
+                if (data.getBooks().get(i).getAuthor().getID() != null) {
+                    Element AID = doc.createElement("uri");
+                    AID.setTextContent(data.getBooks().get(i).getAuthor().getID());
+                    author.appendChild(AID);
+                }
+                entry.appendChild(author);
+            }
+
+            if (data.getBooks().get(i).getLanguage() != null) {
+                Element language = doc.createElement("dcterms:language");
+                language.setTextContent(data.getBooks().get(i).getLanguage());
+                entry.appendChild(language);
+            }
+
+            if (data.getBooks().get(i).getPublisher() != null) {
+                Element publisher = doc.createElement("publisher");
+                publisher.setTextContent(data.getBooks().get(i).getPublisher());
+                entry.appendChild(publisher);
+            }
+
             if(data.getBooks().get(i).getDate() != null){
 			    Element date = doc.createElement("dcterms:issued");
 			    date.setTextContent(Integer.toString(data.getBooks().get(i).getDate().getYear()));
                 entry.appendChild(date);
             }
+
+            if(data.getBooks().get(i).getUpdateTime() != null){
+			    Element update = doc.createElement("updated");
+			    update.setTextContent(data.getBooks().get(i).getUpdateTime());
+                entry.appendChild(update);
+            }
+
             for(int j = 0; j < data.getBooks().get(i).getGenre().size(); ++j){
 			    Element category = doc.createElement("category");
 			    category.setAttribute("term", data.getBooks().get(i).getGenre().get(j));
                 entry.appendChild(category);
             }
-			Element summary = doc.createElement("summary");
-			summary.setTextContent(data.getBooks().get(i).getSummary());
-			Element link1 = doc.createElement("link");
-			link1.setAttribute("type", "application/epub+zip");
-			link1.setAttribute("href", data.getBooks().get(i).getLinks().get("epub"));
-			Element link2 = doc.createElement("link");
-			link2.setAttribute("type", "application/pdf");
-			link2.setAttribute("href", data.getBooks().get(i).getLinks().get("pdf"));
-			Element linkCov = doc.createElement("link");
-			linkCov.setAttribute("type", "image/png");
-            linkCov.setAttribute("rel", "http://opds-spec.org/thumbnail");
-			linkCov.setAttribute("href", data.getBooks().get(i).getImage());
-			
-			entry.appendChild(title);
-			entry.appendChild(id);
-			author.appendChild(name);
-			author.appendChild(AID);
-			entry.appendChild(author);
-			entry.appendChild(language);
-			entry.appendChild(summary);
-			entry.appendChild(link1);
-			entry.appendChild(link2);
-			entry.appendChild(linkCov);
-			
+
+            if (data.getBooks().get(i).getRights() != null) {
+                Element rights = doc.createElement("rights");
+                rights.setTextContent(data.getBooks().get(i).getRights());
+                entry.appendChild(rights);
+            }
+
+            if (data.getBooks().get(i).getSummary() != null) {
+                Element summary = doc.createElement("summary");
+                summary.setTextContent(data.getBooks().get(i).getSummary());
+                entry.appendChild(summary);
+            }
+
+            if (data.getBooks().get(i).getContent() != null) {
+                Element content = doc.createElement("content");
+                content.setTextContent(data.getBooks().get(i).getContent());
+                entry.appendChild(content);
+            }
+
+            if (data.getBooks().get(i).getLinks().get("epub") != null) {
+                Element link1 = doc.createElement("link");
+                link1.setAttribute("type", "application/epub+zip");
+                link1.setAttribute("href", data.getBooks().get(i).getLinks().get("epub"));
+                entry.appendChild(link1);
+            }
+
+            if (data.getBooks().get(i).getLinks().get("pdf") != null) {
+                Element link2 = doc.createElement("link");
+                link2.setAttribute("type", "application/pdf");
+                link2.setAttribute("href", data.getBooks().get(i).getLinks().get("pdf"));
+                entry.appendChild(link2);
+            }
+
+            if (data.getBooks().get(i).getImage() != null) {
+                Element linkCov = doc.createElement("link");
+                linkCov.setAttribute("type", "image/png");
+                linkCov.setAttribute("rel", "http://opds-spec.org/thumbnail");
+                linkCov.setAttribute("href", data.getBooks().get(i).getImage());
+                entry.appendChild(linkCov);
+            }
+
 			root.appendChild(entry);
 		}
 		
