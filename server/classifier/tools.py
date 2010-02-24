66c6e7733ceb187b9d0effde4e15abef794e9d34
Tuzova.K <Tuzova.K@gmail.com>
2010-02-24 10:18:04 +0000

    server: added pdf dump

--- server/classifier/tools.py
+++ server/classifier/tools.py
@@ -8,6 +8,15 @@ import urllib
 from spec.external.pdfminer.pdfinterp import PDFResourceManager, process_pdf
 from spec.external.pdfminer.converter import TextConverter
 from spec.external.pdfminer.layout import LAParams
+from spec.external.pdfminer.pdfparser import PDFDocument, PDFParser
+
+from spec.external.pdfminer.pdftypes import stream_value, PDFStream, PDFObjRef
+from spec.external.pdfminer.psparser import PSKeyword
+#from pdfminer.pdftypes import PDFStream, PDFObjRef, resolve1, stream_value
+
+ESC_PAT = re.compile(r'[\000-\037&<>()\042\047\134\177-\377]')
+def esc(s):
+    return ESC_PAT.sub(lambda m:'&#%d;' % ord(m.group(0)), s)
 
 def read(b, feed, classif):
     ''' gets URL and classify items '''
@@ -141,6 +150,7 @@ def check_fullclassifier(classif):
         counter += read_fullbook(True, ('http://feedbooks.com/books.atom?lang=en&amp;page=%s' % i), classif)
         
     print "Total: ", counter
+    return
 
 def read_pdf(input_file):
     password = ''
@@ -164,3 +174,86 @@ def read_pdf(input_file):
     device.close()
     outfp.close()
     return
+    
+def dump_pdf(input_file, pages):
+    password = ''
+    doc = PDFDocument()
+    fp = file(input_file, 'rb')
+    parser = PDFParser(fp)
+    parser.set_document(doc)
+    doc.set_parser(parser)
+    doc.initialize(password)
+    pagenos = set()
+    pagenos.update( int(x)-1 for x in pages.split(',') )
+    codec = 'text'
+    outfile = input_file[0:-4] + ".txt"
+    outfp = file(outfile, 'w')
+
+    if pagenos:
+        for (pageno,page) in enumerate(doc.get_pages()):
+            if pageno in pagenos:
+                if codec:
+                    for obj in page.contents:
+                        obj = stream_value(obj)
+                        dumpxml(outfp, obj, codec=codec)
+                else:
+                    dumpxml(outfp, page.attrs)
+
+    outfp.write('\n')
+    return
+    
+def dumpxml(out, obj, codec=None):
+    if isinstance(obj, dict):
+        out.write('<dict size="%d">\n' % len(obj))
+        for (k,v) in obj.iteritems():
+            out.write('<key>%s</key>\n' % k)
+            out.write('<value>')
+            dumpxml(out, v)
+            out.write('</value>\n')
+        out.write('</dict>')
+        return
+
+    if isinstance(obj, list):
+        out.write('<list size="%d">\n' % len(obj))
+        for v in obj:
+            dumpxml(out, v)
+            out.write('\n')
+        out.write('</list>')
+        return
+
+    if isinstance(obj, str):
+        out.write('<string size="%d">%s</string>' % (len(obj), esc(obj)))
+        return
+
+    if isinstance(obj, PDFStream):
+        if codec == 'raw':
+            out.write(obj.get_rawdata())
+        elif codec == 'binary':
+            out.write(obj.get_data())
+        else:
+            out.write('<stream>\n<props>\n')
+            dumpxml(out, obj.attrs)
+            out.write('\n</props>\n')
+            if codec == 'text':
+                data = obj.get_data()
+                out.write('<data size="%d">%s</data>\n' % (len(data), esc(data)))
+            out.write('</stream>')
+        return
+
+    if isinstance(obj, PDFObjRef):
+        out.write('<ref id="%d"/>' % obj.objid)
+        return
+
+    if isinstance(obj, PSKeyword):
+        out.write('<keyword>%s</keyword>' % obj.name)
+        return
+
+    if isinstance(obj, PSLiteral):
+        out.write('<literal>%s</literal>' % obj.name)
+        return
+
+    if isinstance(obj, int) or isinstance(obj, float):
+        out.write('<number>%s</number>' % obj)
+        return
+
+    raise TypeError(obj)
