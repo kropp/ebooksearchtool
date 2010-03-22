06bc4b196d6c98f787d2c10005cc916bd9edafd8
Tuzova.K <Tuzova.K@gmail.com>
2010-03-22 16:33:04 +0000

    server: modified admin for books

--- server/book/admin.py
+++ server/book/admin.py
@@ -2,15 +2,20 @@ from book.models import *
 
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('name', )
-    filter_horizontal = ('tag',)
+    filter_horizontal = ('tag', 'book')
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ('title', )
+    filter_horizontal = ('tag', 'annotation', 'book_file')
+
 
 admin.site.register(Annotation)
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 
 admin.site.register(Author, AuthorAdmin)
 
-#admin.site.register(BookFile)
+admin.site.register(BookFile)
 
 #admin.site.register(Series)
 
