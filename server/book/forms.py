aeb97cc4bc1e805df8f3823eddf57cbdc633a5ec
Tuzova.K <Tuzova.K@gmail.com>
2010-04-06 09:04:54 +0000

    server: removed unusable code in form

--- server/book/forms.py
+++ server/book/forms.py
@@ -14,20 +14,25 @@ from itertools import chain
 
 from django.forms import Select
 
-class MyForeignKeyRawIdWidget(forms.Textarea):
+class MyManyToManyRawIdWidget(forms.Textarea):
     """
-    A Widget for displaying ForeignKeys in the "raw_id" interface rather than
-    in a <select> box.
+    A Widget for displaying ManyToMany ids in the "raw_id" interface rather than
+    in a <select multiple> box.
     """
     def __init__(self, rel, attrs=None):
         self.rel = rel
-        super(MyForeignKeyRawIdWidget, self).__init__(attrs)
+        super(MyManyToManyRawIdWidget, self).__init__(attrs)
 
     def render(self, name, value, attrs=None):
+        attrs['class'] = 'vManyToManyRawIdAdminField'
+        if value:
+            value = ',\n'.join([str(Book.objects.get(id=v)) for v in value])
+        else:
+            value = ''
         if attrs is None:
             attrs = {}
+        
         related_url = '../../../%s/%s/' % (self.rel.to._meta.app_label, self.rel.to._meta.object_name.lower())
-#        related_url = '../../../%s/%s/pop' % (self.rel.to._meta.app_label, self.rel.to._meta.object_name.lower())
         params = self.url_parameters()
         if params:
             url = '?' + '&amp;'.join(['%s=%s' % (k, v) for k, v in params.items()])
@@ -35,15 +40,20 @@ class MyForeignKeyRawIdWidget(forms.Textarea):
             url = ''
         if not attrs.has_key('class'):
             attrs['class'] = 'vForeignKeyRawIdAdminField' # The JavaScript looks for this hook.
-        output = [super(MyForeignKeyRawIdWidget, self).render(name, value, attrs)]
-        # TODO: "id_" is hard-coded here. This should instead use the correct
-        # API to determine the ID dynamically.
+        output = [super(MyManyToManyRawIdWidget, self).render(name, value, attrs)]
+
         output.append('<a href="%s%s" class="related-lookup" id="lookup_id_%s" onclick="return showRelatedObjectLookupPopup(this);"> ' % \
             (related_url, url, name))
         output.append('<img src="%simg/admin/selector-search.gif" width="16" height="16" alt="%s" /></a>' % (settings.ADMIN_MEDIA_PREFIX, _('Lookup')))
         if value:
             output.append(self.label_for_value(value))
         return mark_safe(u''.join(output))
+
+    def url_parameters(self):
+        return self.base_url_parameters()
+
+    def label_for_value(self, value):
+        return ''
 
     def base_url_parameters(self):
         params = {}
@@ -58,47 +68,8 @@ class MyForeignKeyRawIdWidget(forms.Textarea):
             params.update(dict(items))
         return params
 
-    def url_parameters(self):
-        from django.contrib.admin.views.main import TO_FIELD_VAR
-        params = self.base_url_parameters()
-        params.update({TO_FIELD_VAR: self.rel.get_related_field().name})
-        return params
-
-    def label_for_value(self, value):
-        key = self.rel.get_related_field().name
-        obj = self.rel.to._default_manager.get(**{key: value})
-        return '&nbsp;<strong>%s</strong>' % escape(truncate_words(obj, 14))
-
-class MyManyToManyRawIdWidget(MyForeignKeyRawIdWidget):
-    """
-    A Widget for displaying ManyToMany ids in the "raw_id" interface rather than
-    in a <select multiple> box.
-    """
-    def __init__(self, rel, attrs=None):
-#        print 'my'
-#        print rel
-        super(MyManyToManyRawIdWidget, self).__init__(rel, attrs)
-
-    def render(self, name, value, attrs=None):
-        attrs['class'] = 'vManyToManyRawIdAdminField'
-        if value:
-            value = ',\n'.join([str(Book.objects.get(id=v)) for v in value])
-        else:
-            value = ''
-        return super(MyManyToManyRawIdWidget, self).render(name, value, attrs)
-
-    def url_parameters(self):
-        return self.base_url_parameters()
-
-    def label_for_value(self, value):
-        return ''
-
     def value_from_datadict(self, data, files, name):
         value = data.get(name, None)
-#        print 'value'
-#        print value
-#        for v in value
-#        value = Book.objects.get(id=v)
         if value and ',' in value:
             return data[name].split(',')
         if value:
