b2783752b68f4cf5a2034f736b4e0a288d5cffa0
Tuzova.K <Tuzova.K@gmail.com>
2010-05-30 12:27:23 +0000

    server: fixed w3org comments

---
+++ server/media/js/views/catalog.js
@@ -0,0 +1,17 @@
+function checkForm(form) {
+    var errorList = [];    
+    for (var i = 0; i < form.elements.length; i++) {
+    el = form.elements[i];
+    elName = el.nodeName.toLowerCase();
+    value = el.value;
+    if (elName == "input") {
+        type = el.type.toLowerCase();
+        if( type == "text" && value == "") {
+                errorList.push(1);
+        }
+    }
+    }
+if (!errorList.length) return true;
+return false;
+
+}
