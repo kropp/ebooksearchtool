ea5ab496e3825c9148fc6b341fd9a73ef447c32e
Tuzova.K <Tuzova.K@gmail.com>
2010-05-30 12:48:50 +0000

    server: some fixes in xml scheme

--- server/media/js/views/catalog.js
+++ server/media/js/views/catalog.js
@@ -15,3 +15,27 @@ if (!errorList.length) return true;
 return false;
 
 }
+
+function checkExtForm(form) {
+    var errorList = [];    
+    for (var i = 0; i < form.elements.length; i++) {
+    el = form.elements[i];
+    elName = el.nodeName.toLowerCase();
+    value = el.value;
+    if (elName == "input") {
+        type = el.type.toLowerCase();
+        if( type == "text" && value == "") {
+                errorList.push(1);
+        }
+    }
+    if (elName == "select") {
+        if( value == "") {
+                errorList.push(1);
+        }
+    }
+
+    }
+if (errorList.length < 4) return true;
+return false;
+
+}
